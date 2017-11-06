fddceea55e2377c8316b98516f765bf5cd6b28d6
Lou van der Laarse <loul@tamtam.nl>
2017-11-06 13:35:04 +0100

    Format EmbedImageExtension file

--- src/Twig/EmbedImageExtension.php
+++ src/Twig/EmbedImageExtension.php
@@ -17,136 +17,141 @@ use Drupal\image\Entity\ImageStyle;
  * Print a menu
  * @package Drupal\embed_view\Twig
  */
-class EmbedImageExtension extends \Twig_Extension {
-  protected $imageFactory;
-  protected $renderer;
+class EmbedImageExtension extends \Twig_Extension
+{
+    protected $imageFactory;
+    protected $renderer;
 
 
-  /**
-   * EmbedImageExtension constructor.
-   * @param \Drupal\embed_image\Twig\ImageFactory $imageFactory
-   * @param \Drupal\embed_image\Twig\RendererInterface $renderer
-   */
-  public function __construct() {
-    $this->imageFactory = \Drupal::service('image.factory');
-    $this->renderer = \Drupal::service('renderer');
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getName() {
-    return 'embed_image';
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getFunctions() {
-    return [
-      new \Twig_SimpleFunction('embed_responsive_image', [$this, 'embedRepsonsiveImage'], [
-        'is_safe' => ['html'],
-      ]),
-      new \Twig_SimpleFunction('embed_image', [$this, 'embedImage'], [
-        'is_safe' => ['html'],
-      ]),
-        new \Twig_SimpleFunction('img_style_url', [$this, 'img_style_url'], [
-            'is_safe' => ['html'],
-        ]),
-    ];
-  }
-
-
-  /**
-   * @param $target_id
-   * @param string $responsive_image_style_id
-   * @return array
-   */
-  public function embedRepsonsiveImage($target_id, $responsive_image_style_id = '') {
-    $file = File::load($target_id);
-
-    if( $file ) {
-      $variables = [
-        'responsive_image_style_id' => $responsive_image_style_id,
-        'uri'                       => $file->getFileUri(),
-      ];
-
-      $image = $this->imageFactory->get($file->getFileUri());
-
-      if( $image->isValid() ) {
-        $variables['width'] = $image->getWidth();
-        $variables['height'] = $image->getHeight();
-      }
-      else {
-        $variables['width'] = $variables['height'] = NULL;
-      }
-
-      $responsiveImage = [
-        '#theme'                     => 'responsive_image',
-        '#width'                     => $variables['width'],
-        '#height'                    => $variables['height'],
-        '#responsive_image_style_id' => $variables['responsive_image_style_id'],
-        '#uri'                       => $variables['uri'],
-      ];
-
-
-      $this->renderer->addCacheableDependency($responsiveImage, $file);
-
-      return [$responsiveImage];
+    /**
+     * EmbedImageExtension constructor.
+     * @param \Drupal\embed_image\Twig\ImageFactory $imageFactory
+     * @param \Drupal\embed_image\Twig\RendererInterface $renderer
+     */
+    public function __construct()
+    {
+        $this->imageFactory = \Drupal::service('image.factory');
+        $this->renderer = \Drupal::service('renderer');
     }
 
-    return [];
-  }
-
-  /**
-   * @param $target_id
-   * @param string $image_style
-   * @return array
-   */
-  public function embedImage($target_id, $image_style = 'thumbnail') {
-    $file = File::load($target_id);
-
-    if( $file ) {
-      $variables = array(
-        'style_name' => $image_style,
-        'uri'        => $file->getFileUri(),
-      );
-
-      $image = $this->imageFactory->get($file->getFileUri());
-
-      if( $image->isValid() ) {
-        $variables['width'] = $image->getWidth();
-        $variables['height'] = $image->getHeight();
-      }
-      else {
-        $variables['width'] = $variables['height'] = NULL;
-      }
-
-      $imageBuild = [
-        '#theme'      => 'image_style',
-        '#width'      => $variables['width'],
-        '#height'     => $variables['height'],
-        '#style_name' => $variables['style_name'],
-        '#uri'        => $variables['uri'],
-      ];
-
-      $this->renderer->addCacheableDependency($imageBuild, $file);
-
-      return [$imageBuild];
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'embed_image';
     }
 
-    return [];
-  }
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('embed_responsive_image', [$this, 'embedRepsonsiveImage'], [
+                'is_safe' => ['html'],
+            ]),
+            new \Twig_SimpleFunction('embed_image', [$this, 'embedImage'], [
+                'is_safe' => ['html'],
+            ]),
+            new \Twig_SimpleFunction('img_style_url', [$this, 'img_style_url'], [
+                'is_safe' => ['html'],
+            ]),
+        ];
+    }
+
+
+    /**
+     * @param $target_id
+     * @param string $responsive_image_style_id
+     * @return array
+     */
+    public function embedRepsonsiveImage($target_id, $responsive_image_style_id = '')
+    {
+        $file = File::load($target_id);
+
+        if ($file) {
+            $variables = [
+                'responsive_image_style_id' => $responsive_image_style_id,
+                'uri' => $file->getFileUri(),
+            ];
+
+            $image = $this->imageFactory->get($file->getFileUri());
+
+            if ($image->isValid()) {
+                $variables['width'] = $image->getWidth();
+                $variables['height'] = $image->getHeight();
+            } else {
+                $variables['width'] = $variables['height'] = NULL;
+            }
+
+            $responsiveImage = [
+                '#theme' => 'responsive_image',
+                '#width' => $variables['width'],
+                '#height' => $variables['height'],
+                '#responsive_image_style_id' => $variables['responsive_image_style_id'],
+                '#uri' => $variables['uri'],
+            ];
+
+
+            $this->renderer->addCacheableDependency($responsiveImage, $file);
+
+            return [$responsiveImage];
+        }
+
+        return [];
+    }
 
     /**
      * @param $target_id
      * @param string $image_style
      * @return array
      */
-    public function getImageStyleUrl($imageUri, $image_style = 'thumbnail') {
+    public function embedImage($target_id, $image_style = 'thumbnail')
+    {
+        $file = File::load($target_id);
+
+        if ($file) {
+            $variables = array(
+                'style_name' => $image_style,
+                'uri' => $file->getFileUri(),
+            );
+
+            $image = $this->imageFactory->get($file->getFileUri());
+
+            if ($image->isValid()) {
+                $variables['width'] = $image->getWidth();
+                $variables['height'] = $image->getHeight();
+            } else {
+                $variables['width'] = $variables['height'] = NULL;
+            }
+
+            $imageBuild = [
+                '#theme' => 'image_style',
+                '#width' => $variables['width'],
+                '#height' => $variables['height'],
+                '#style_name' => $variables['style_name'],
+                '#uri' => $variables['uri'],
+            ];
+
+            $this->renderer->addCacheableDependency($imageBuild, $file);
+
+            return [$imageBuild];
+        }
+
+        return [];
+    }
+
+    /**
+     * @param $target_id
+     * @param string $image_style
+     * @return array
+     */
+    public function getImageStyleUrl($imageUri, $image_style = 'thumbnail')
+    {
         $url = null;
 
-        if( empty($imageUri) === false ) {
+        if (empty($imageUri) === false) {
             $url = ImageStyle::load($image_style)->buildUrl($imageUri);
 
             return $url;
